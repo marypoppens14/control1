5ad6fff7fcc3d0ede0ad5f81115053c90d22afaf
Maria Popova <marypoppens14@gmail.com>
2023-06-27 17:35:50 +0300

    Добавила программу

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам
+
+string[] array = new string[5] {"cat", "palace", "on", "garden", "to"};
+string[] new_array = new string[array.Length];
+int j = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+        {
+        new_array[j] = array[i];
+        j++;
+        }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
+PrintArray(new_array);
